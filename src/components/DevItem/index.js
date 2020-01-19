c230cab14803923db0495b7eca26694e24531ee8
Diguix <rtteixeira1@gmail.com>
2020-01-19 17:38:00 -0300

    add component DevForm

---
+++ src/components/DevItem/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+
+function DevItem({ dev }) {
+    return (
+        <li className="dev-item">
+            <header>
+                <img src={dev.avatar_url} alt={dev.name}></img>
+                <div className="user-info">
+                    <strong>{dev.name}</strong>
+                    <span>{dev.techs.join(', ')}</span>
+
+                    <p>{dev.bio}</p>
+                    <a href={`https://github.com/${dev.github_username}`}>
+                        Acessar perfil no github
+                    </a>
+                </div>
+            </header>
+        </li>
+    );
+}
+
+export default DevItem;
